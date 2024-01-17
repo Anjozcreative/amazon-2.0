fc45cd2c9ec6cf5c1e68724283a8dea87ac45cd2
Anjoz creative <anjozcreative@gmail.com>
2024-01-17 17:48:02 +0100

    Update productFeed.js

--- app/components.js/ProductFeed.js
+++ app/components.js/ProductFeed.js
@@ -4,17 +4,56 @@ function ProductFeed({ products }) {
 
   return (
     <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-48 mx-auto">
-      {products.map(({ id, title, price, description, category, image }) => (
-        <Product
-        key={id}
-        id={id}
-        title={title}
-        price={price}
-        description={description}
-        category={category}
-        image={image}
-        />
-      ))}
+      {/**the first four products */}
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      {/**Ads Banner */}
+      <img
+        className="md:col-span-full "
+        src="https://links.papareact.com/dyz"
+        alt="advert banner"
+      />
+      {/**Two more products */}
+      <div className="md:col-span-2 ">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {/**The rest of the products */}
+        {products
+          .slice(5, products.length)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
     </div>
   )
 };
